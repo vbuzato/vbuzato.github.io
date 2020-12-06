daecd8bb2b64f88666cc6f2db9f0186be976c104
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-12-06 19:35:05 -0300

    Projeto iniciado

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "jest": true,
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    // "extends": [
+    //     "eslint:recommended",
+    //     "plugin:react/recommended"
+    // ],
+    "extends": ["airbnb", "airbnb/hooks"] ,
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        // suppress errors for missing 'import React' in files
+       "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+      }
+};
