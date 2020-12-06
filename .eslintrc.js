55daa60f386717f82bc98b138124d79f19ad5ee8
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-12-06 19:53:39 -0300

    ESLint Configurado

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    "env": {
-        "jest": true,
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    jest: true,
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    // "extends": [
-    //     "eslint:recommended",
-    //     "plugin:react/recommended"
-    // ],
-    "extends": ["airbnb", "airbnb/hooks"] ,
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        // suppress errors for missing 'import React' in files
-       "react/react-in-jsx-scope": "off",
-        // allow jsx syntax in js files (for next.js project)
-       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
-      }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
+  },
 };
+
+// "extends": [
+//     "eslint:recommended",
+//     "plugin:react/recommended"
+// ],
