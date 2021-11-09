bf69a6789e55403638cdde3d7fbec50d0e80022c
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-11-09 13:23:29 -0300

    feat: sobre mim

--- src/pages/About.js
+++ src/pages/About.js
@@ -36,27 +36,10 @@ export default function About() {
               Sobre
             </h1>
             <p className="text-content">
-              &quot;Sou
-              estudante de desenvolvimento de software na Trybe, onde já concluí o módulo
-              de front-end e estou cursando back-end (1 ano de curso intensivo). Também
-              estou cursando Análise e Desenvolvimento de Sistemas na Uninter, no 1° ano
-              de curso.
-              <br />
-              <br />
-              O que eu pretendo fazer pelo resto dos meus dias?
-              <br />
-              <br />
-              Foi o que me perguntei depois de 9 anos de carreira na área da qualidade,
-              na indústria automotiva. Estava estabilizado, mas não 100% feliz. Decidi
-              transformar o hobbie, que me fazia viajar por horas atrás de soluções, em
-              profissão. Decidi estudar desenvolvimento de software porque isso sempre
-              esteve presente em minha vida, mas não na intensidade que eu desejava. Me
-              planejei e pedi demissão da antiga vida para fazer o que mais gosto.
-              <br />
-              <br />
-              Hoje posso dizer que acertei o caminho e a decisão. Agora sigo em busca de
-              oportunidades onde eu possa levar essa motivação e paixão para ajudar a
-              transformar e somar dentro da equipe que eu estiver inserido.&quot;
+              Front-end por 5 anos e 2 anos de desenvolvimento fullstack, mais 9 anos de
+              profissão na indústria automotiva (metrologia e geometria). Hoje, sigo
+              ampliando conhecimentos nessa motivadora jornada de desenvolvimento de
+              software.
               <br />
               <br />
               <strong className="name">Vinicius Buzato</strong>
